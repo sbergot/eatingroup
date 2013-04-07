1481dd0af78632a4ad830c356ea75ddd3bfe3a20
Simon Bergot <simon.bergot@gmail.com>
2013-04-07 21:41:08 +0200

    initial commit

---
+++ src/eatingroup/core.clj
@@ -0,0 +1,22 @@
+(ns eatingroup.core
+  (:use lamina.core
+        aleph.http
+        compojure.core)
+  (:require [compojure.route :as route]))
+
+(defn hello-world [channel request]
+  (let [params (:route-params request)
+        id (:id params)]
+    (enqueue channel
+             {:status 200
+              :headers {"content-type" "text/html"}
+              :body (str "Hello World! " id)})))
+
+(defroutes app-routes
+  (GET "/" [] "hello-world")
+  (GET "/toto/:id" [id] (wrap-aleph-handler hello-world))
+  (route/resources "/static")
+  (route/not-found "page not found"))
+
+(defn -main []
+  (start-http-server (wrap-ring-handler app-routes) {:port 8008}))
